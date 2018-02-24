309ccba1604db88f69d0671514410b6654d1a8c7
Chris J Arges <christopherarges@gmail.com>
2018-02-24 15:49:28 -0600

    Don't use frozenset, detect minor scales properly.

--- jamulizer/chords.py
+++ jamulizer/chords.py
@@ -22,36 +22,36 @@ class Chords:
 
     """ scale intervals """
     scales = {
-        frozenset([0, 2, 4, 5, 7, 9, 11]): 'major',
-        frozenset([0, 2, 4, 3, 7, 9, 11]): 'minor',
+        (0, 2, 4, 5, 7, 9, 11): 'major',
+        (0, 2, 3, 5, 7, 8, 10): 'minor'
     }
 
     """ chord intervals """
     chords = {
-        frozenset([0, 7]): ['5', 'fifth'],
-        frozenset([0, 2, 7]): ['sus2', 'suspended second'],
-        frozenset([0, 5, 7]): ['sus4', 'suspended fourth'],
-        frozenset([0, 4, 7]): ['maj', 'major'],
-        frozenset([0, 3, 7]): ['min', 'minor'],
-        frozenset([0, 3, 6]): ['dim', 'diminished'],
-        frozenset([0, 4, 8]): ['aug', 'augmented'],
-        frozenset([0, 3, 6, 9]): ['dim7', 'diminished seventh'],
-        frozenset([0, 3, 6, 10]): ['m7b5', 'half-diminished seventh'],
-        frozenset([0, 3, 7, 10]): ['min7', 'minor seventh'],
-        frozenset([0, 3, 7, 11]): ['minmaj7', 'minor major seventh'],
-        frozenset([0, 4, 7, 10]): ['7', 'dominant seventh'],
-        frozenset([0, 4, 7, 11]): ['maj7', 'major seventh'],
-        frozenset([0, 4, 8, 10]): ['aug7', 'augmented seventh'],
-        frozenset([0, 4, 8, 11]): ['maj7+5', 'augmented major seventh'],
-        frozenset([0, 4, 8, 10, 2]): ['9', 'dominant ninth'],
-        frozenset([0, 4, 8, 10, 2, 5]): ['11', 'dominant eleventh'],
-        frozenset([0, 4, 8, 10, 2, 5, 9]): ['13', 'dominant thirteenth'],
+        (0, 7): ['5', 'fifth'],
+        (0, 2, 7): ['sus2', 'suspended second'],
+        (0, 5, 7): ['sus4', 'suspended fourth'],
+        (0, 4, 7): ['maj', 'major'],
+        (0, 3, 7): ['min', 'minor'],
+        (0, 3, 6): ['dim', 'diminished'],
+        (0, 4, 8): ['aug', 'augmented'],
+        (0, 3, 6, 9): ['dim7', 'diminished seventh'],
+        (0, 3, 6, 10): ['m7b5', 'half-diminished seventh'],
+        (0, 3, 7, 10): ['min7', 'minor seventh'],
+        (0, 3, 7, 11): ['minmaj7', 'minor major seventh'],
+        (0, 4, 7, 10): ['7', 'dominant seventh'],
+        (0, 4, 7, 11): ['maj7', 'major seventh'],
+        (0, 4, 8, 10): ['aug7', 'augmented seventh'],
+        (0, 4, 8, 11): ['maj7+5', 'augmented major seventh'],
+        (0, 4, 8, 10, 2): ['9', 'dominant ninth'],
+        (0, 4, 8, 10, 2, 5): ['11', 'dominant eleventh'],
+        (0, 4, 8, 10, 2, 5, 9): ['13', 'dominant thirteenth'],
     }
 
 
     def name_scale(self, input_notes):
         for key in self.notes:
-            shift_scale = frozenset([(note-key)%12 for note in input_notes])
+            shift_scale = tuple(set([(note-key)%12 for note in input_notes]))
             try:
                 return self.notes[key] + " " + self.scales[shift_scale]
             except KeyError:
@@ -77,8 +77,8 @@ class Chords:
     def name_chord(self, input_notes):
         for key in self.notes:
             try:
-                note_set = frozenset([((note%12)-key)%12 for note in input_notes])
-                return self.notes[key] + " " + self.chords[note_set][0]
+                note_tuple = tuple(set([((note%12)-key)%12 for note in input_notes]))
+                return self.notes[key] + " " + self.chords[note_tuple][0]
             except KeyError:
                 pass
             except TypeError:
--- test/test_chords.py
+++ test/test_chords.py
@@ -17,8 +17,9 @@ def test_name_notes(test_input, expected):
 
 
 CHORD_INPUTS = [
-    (frozenset([0, 4, 7]), 'C maj'),
-    (frozenset([2, 5, 9]), 'D min'),
+    ([0, 7], 'C 5'),
+    ([0, 4, 7], 'C maj'),
+    ([2, 5, 9], 'D min'),
     (['a', 2, 3], None),
     (None, None),
     ([1, 2, 3], None)
@@ -28,4 +29,16 @@ CHORD_INPUTS = [
 def test_name_chords(test_input, expected):
     chords = Chords()
     actual = chords.name_chord(test_input)
+    assert actual == expected
+
+
+SCALE_INPUTS = [
+    ([0, 2, 4, 5, 7, 9, 11], 'C major'),
+    ([0, 2, 3, 5, 7, 8, 10], 'C minor')
+]
+
+@pytest.mark.parametrize("test_input, expected", SCALE_INPUTS)
+def test_name_scale(test_input, expected):
+    chords = Chords()
+    actual = chords.name_scale(test_input)
     assert actual == expected
